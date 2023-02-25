5602bc4e8fc704192d8a07a543e16617f54fef4b
Kritika Rana <112557173+kritikarana06@users.noreply.github.com>
2023-02-25 20:17:09 +0530

    Add files via upload

---
+++ k.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,f=1;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    while(n>0){
+        f=f*n;
+        
+    }    
+     printf("the factorial is %d",&f);
+
+    return 0;
+}
+    
